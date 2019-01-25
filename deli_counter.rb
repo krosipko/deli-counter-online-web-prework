14715754ff918b598f28cc8f40ec6fed3e8685f4
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:07:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,11 @@ def line(people)
   katz_deli << people
   new_array = ["The line is currently: "]
   if katz_deli.size > 0 
-    
+    katz_deli.each_with_index do |new_array, index|
+      place = katz_deli[index]
+      new_array << "#{index}. #{place} "
+    end
+    new_array
   else
     puts "The line is currently empty."
   end 
