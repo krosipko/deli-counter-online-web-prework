fb03c096ca21f8b0bc4f78206c7e065bc94bb683
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:11:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(people)
     #  place = katz_deli[index]
    #   new_array << "#{index}. #{place} "
    # end
-   # new_array
+   new_array
   else
     puts "The line is currently empty."
   end 
