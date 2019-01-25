be34f9decfb58512e680585ed2b1da4e1fb9dd23
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 05:11:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, new_person)
   katz_deli << new_person
-  puts "Welcome, #{new_person}. You are number #{katz_deli.size} in line"
+  puts "Welcome, #{new_person}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving
