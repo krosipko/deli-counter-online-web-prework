c3beb30439b473fe9e2d9aeca96c0dee28b22a1b
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:26:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     katz_deli.each_with_index |place, index|
     place = katz_deli[index]
     new_deli_lol << "#{index}. #{place}"
-    
+    puts new_deli_lol
   else 
     puts "The line is currently empty."
   end
