12c755f2fc7dc68490deebaca903b0c7eb7b599b
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:46:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   new_deli_lol << 
   if katz_deli.size > 0 
     katz_deli.each_with_index do |name, index|
-      new_deli_lol << "#{index + 1}. #{name}"
+      new_deli_lol << "#{index + 1}. #{name.comp}"
     end
     puts new_deli_lol
   else 
