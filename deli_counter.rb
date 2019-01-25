8d0a92c749a48b7be1b457324ccebe4b431e9650
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:13:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,5 @@
 def line(people) 
-  katz_deli = []
-  katz_deli << people
-  new_array = ["The line is currently: "]
-  if katz_deli.size > 0 
-  else
-    puts "The line is currently empty."
-  end 
+  katz_deli = [people]
 end
 
 def take_a_number
