a83c6ade3d763e4dc310a7275e3987681b897a09
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 04:25:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 katz_deli = []
 def line(katz_deli) 
-  new_deli_lol = []
+  new_deli_lol = ["The line is currently: "]
   if katz_deli.size > 0 
+    katz_deli.each_with_index |place, index|
+    place = katz_deli[index]
+    new_deli_lol << "#{index}. #{place}"
     
   else 
     puts "The line is currently empty."
