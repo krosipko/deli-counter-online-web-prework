aa4f19d5f334c0755de5900d441d6603ad26f8df
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 05:14:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,10 @@ def take_a_number(katz_deli, new_person)
 end
 
 def now_serving
+  if katz_deli.size > 0 
+    puts katz_deli[0]
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
   
 end
