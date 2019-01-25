0002f214bf1f96910ce8f7934b648a467925ccaf
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 05:24:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,6 @@ def now_serving(katz_deli)
   if katz_deli.size > 0 
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
-  #  return katz_deli
   else
     puts "There is nobody waiting to be served!"
   end
