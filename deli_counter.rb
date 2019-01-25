a83311cd36191166df2e64f548740853e6b2f628
Kyle Rosipko <krosipko@outlook.com>
2019-01-25 03:58:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(people) 
+  katz_deli = []
+  katz_deli << people
+  if katz_deli.size > 0 
+  
+  else
+    puts 
+end
+
+def take_a_number
+  
+end
+
+def now_serving
+  
+end
